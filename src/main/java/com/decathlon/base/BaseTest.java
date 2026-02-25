e3a7e8c88744119b0914466ee706984a25515ec8
2464331 <VishalShirish.Lalge@cognizant.com>
2026-02-25 10:35:15 +0530

    first test case

--- src/main/java/com/decathlon/base/BaseTest.java
+++ src/main/java/com/decathlon/base/BaseTest.java
@@ -1,4 +1,27 @@
 package com.decathlon.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
 public class BaseTest {
+    public WebDriver driver;
+
+
+    public void setup() {
+        driver = new ChromeDriver(); // Initialize driver
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.decathlon.in/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
--- src/test/java/com/decathlon/testng/test/LoginFunctionalTest.java
+++ src/test/java/com/decathlon/testng/test/LoginFunctionalTest.java
@@ -1,4 +1,25 @@
 package com.decathlon.testng.test;
 
-public class LoginFunctionalTest {
+import com.decathlon.base.BaseTest;
+import com.decathlon.pages.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginFunctionalTest extends BaseTest {
+
+    HomePage home;
+    @BeforeClass
+    public void pageInitiation(){
+        setup();
+        home = new HomePage(driver);
+
+    }
+
+    @Test
+    public void pageTitleAndLogoVerification(){
+        home.homePageVerification();
+    }
+
 }
